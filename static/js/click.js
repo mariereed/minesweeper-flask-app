ad2373865b59bbe2e6893406ccd4e1a17db59ba9
marie <catmariereed@gmail.com>
2018-05-22 16:22:43 -0700

    Skelton for JS for handling clicks.

---
+++ static/js/click.js
@@ -0,0 +1,35 @@
+"use strict";
+
+$('document').ready(function() {
+
+    // EXAMPLE for how to handle revealTile/flagTile
+    
+    // function changeButton(results) {
+    //     if (results.confirm === true) {
+    //         let theLikeButton = $('#' + String(results.id));
+    //         theLikeButton.attr('style', "color:red");
+    //     }
+    // }
+
+    // Listen for clicks, route appropriately.
+    $('#tile').mousedown(function(event) {
+        switch (event.which) {
+            case 1:
+                // Left Mouse button pressed
+                // Reveal the tile
+                // $.post('/reveal', {'x': this.x, 'y': this.y}, revealTile);
+                break;
+            case 2:
+                // Middle Mouse button pressed
+                // Do nothing
+                break;
+            case 3:
+                // Right Mouse button pressed
+                // Flag/Unflag the tile
+                // $.post('/flag', {'x': this.x, 'y': this.y}, flagTile);
+                break;
+            default:
+                // Do nothing
+        }
+    });
+});
