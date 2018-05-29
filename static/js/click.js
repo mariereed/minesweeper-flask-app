b2d52296ce9bbb6c55c3803791a61886cbfd3a8e
marie <catmariereed@gmail.com>
2018-05-29 14:33:24 -0700

    Properly identified changes in board.

--- static/js/click.js
+++ static/js/click.js
@@ -4,12 +4,30 @@ $('document').ready(function() {
     function revealTile(results) {
         if (results.confirm === true) {
             console.log('Success Reveal');
-            // For each tile in results.board that does not have a '?' value...
-            // change the tile to its corresponding value (5,4,3,2,1,blank)
-            let fill = "(0, 0)";
-            let theDiv = document.getElementById(fill);
-            let content = document.createTextNode("?");
-            theDiv.appendChild(content);
+
+            let height = results.board.length;
+            let width  = results.board[0].length;
+
+            let theBoard = document.getElementById("board");
+
+            for (let i = 0; i < width; i++) {
+                for (let j = 0; j < height; j++) {
+                    if (results.board[i][j] !== "?") {
+                        // fill in value for that tile
+                        console.log('I found a value');
+                        console.log(i, j);
+                        console.log(results.board[i][j]);
+                    }
+                }
+            }
+
+            // // For each tile in results.board that does not have a '?' value...
+            // // change the tile to its corresponding value (5,4,3,2,1,blank)
+            // let coords = "(0, 0)";
+            // let value = "?";
+            // let theDiv = document.getElementById(coords);
+            // let content = document.createTextNode(value);
+            // theDiv.appendChild(content);
         }
         else {
             // reveal mines with current board
