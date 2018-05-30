8a1a1b32a17e94cff2adc3087eb65d341bf0a2d3
marie <catmariereed@gmail.com>
2018-05-29 18:19:17 -0700

    Added Timer js file.

---
+++ static/js/timer.js
@@ -0,0 +1,11 @@
+"use strict";
+
+$('document').ready(function() {
+    
+    let board = document.getElementById('board');
+
+    board.addEventListener("click", function(event) {
+        console.log('made it');
+    });
+
+});
