7f165de9d3875906a51ea4108ea3b04eaac675ba
marie <catmariereed@gmail.com>
2018-05-30 16:00:33 -0700

    Added skeleton for timer.

--- static/js/timer.js
+++ static/js/timer.js
@@ -2,10 +2,23 @@
 
 $('document').ready(function() {
     
+    var seconds = 0, minutes = 0, hours = 0, t;
+    let timerDiv = document.getElementById('timer');
+
+
+
+
     let board = document.getElementById('board');
 
     board.addEventListener("click", function(event) {
-        console.log('made it');
+        if (board.classList.contains('started')) {
+            return;
+        }
+        board.classList.add('started');
+        console.log('timer started');
+        // var start = new time
+        // var elapsed = timeNow - start
+        // display time every one second.
     });
 
 });
